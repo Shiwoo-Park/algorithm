afd5c39ca9b543196f79a6e6f47fe595ee1cd8ad
silva23 <woozin23@gmail.com>
2015-08-10 23:27:08 +0900

    renamed

--- _solution_tester.py
+++ _solution_tester.py
@@ -1,1 +1,14 @@
-__author__ = 'Jsilva'
+import os
+from n_queens import Solution
+
+if __name__ == "__main__":
+	s = Solution()
+	rets = s.solveNQueens(5)
+	for ret in rets:
+		for row in ret:
+			print row
+		print "====================="
+
+	# files = os.listdir(".")
+	# for file in files:
+	# 	os.rename(file, file.replace("-", "_"))
---
+++ n_queens_ii.py
@@ -0,0 +1,59 @@
+# https://leetcode.com/problems/n-queens-ii/
+
+class Solution:
+
+	# @param {integer} n
+	# @return {integer}
+	def totalNQueens(self, n):
+		answer_list = self.solveNQueens(n)
+		return len(answer_list)
+
+	# @param {integer} n
+	# @return {string[][]}
+	def solveNQueens(self, n):
+		if n == 1:
+			return [["Q"]]
+		if n < 4:
+			return []
+
+		ret_list = []
+		self.putNextQueen(ret_list, [], {}, n)
+		return ret_list
+
+	def putNextQueen(self, ret_list, now_ret, queen_pos_dic, n):
+		rows = len(now_ret)
+		if rows == n:
+			ret_list.append(now_ret)
+			return
+
+		for i in range(n):
+			check_point = (rows, i)
+			if self.isAvailable(check_point, queen_pos_dic):
+				row = []
+				for k in range(n):
+					if k == i:
+						row.append("Q")
+					else:
+						row.append(".")
+				new_ret = list(now_ret)
+				new_ret.append("".join(row))
+				new_pos_dic = dict(queen_pos_dic)
+				new_pos_dic[rows] = i
+				self.putNextQueen(ret_list, new_ret, new_pos_dic, n)
+
+	def isAvailable(self, chk_point, queen_pos_dic):
+		ret = True
+		for row, col in queen_pos_dic.items():
+			if self.canKill(chk_point, (row, col)):
+				ret = False
+				break
+		return ret
+
+	def canKill(self, point1, point2):
+		if point1[0] == point2[0]:
+			return True
+		if point1[1] == point2[1]:
+			return True
+		if abs(point1[0] - point2[0]) == abs(point1[1] - point2[1]):
+			return True
+		return False
