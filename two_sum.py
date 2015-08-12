1ca795a21040df26ff1283e57c59994371549567
jsilva <woozin23@gmail.com>
2015-08-12 14:01:52 +0900

    mod two sum

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,28 @@
-__author__ = 'jsilva'
+class Solution:
+	# @param {integer[]} nums
+	# @param {integer} target
+	# @return {integer[]}
+	def twoSum(self, nums, target):
+		l = len(nums)
+
+		dic = dict()
+		for i in range(l):
+			num = nums[i]
+			if num not in dic:
+				dic[num] = i
+
+		for i in range(l):
+			num = nums[i]
+			other = target - num
+			if other in dic:
+				if dic[other] != i:
+					ret = [dic[other]+1, i+1]
+					ret.sort()
+					return ret
+
+		return []
+
+if __name__ == "__main__":
+	data = ( [3,2,4], 6 )
+	s = Solution()
+	print s.twoSum(data[0], data[1])
