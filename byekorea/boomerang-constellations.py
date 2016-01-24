0700af8ea3dff912d63cc8a17c36679a3efd3c16
Oniondev <Oniondev>
2016-01-25 02:14:43 +0900

    added files

---
+++ byekorea/boomerang-constellations.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# https://www.facebook.com/hackercup/problem/910374079035613/
+
+# input : list of points
+# return : count of boomerang constellation
+# 부메랑 별자리 : 3개의 점은 연결했을때, 중간 점으로부터 나머지 두개의 점까지 이르는 거리가 같으면 성립
+
+def cont_boomerang_constellation(point_list):
+
+    return 0
---
+++ byekorea/social-network-traversal.py
@@ -0,0 +1,9 @@
+# Amazon problem
+# Each person has his attended course and his direct friend
+
+# getDirectFriendsForUser(userID)
+# getAttendedCoursesForUser(userID)
+# consider 2 depth friends, 
+
+def get_user_courses(user_id) :
+    return []
