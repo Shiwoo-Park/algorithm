3b8c7e6df62dc0c306de01b786ee36296f74832d
shiwoopark <shiwoopark@linecorp.com>
2016-01-25 16:15:48 +0900

    mod

--- utils/models.py
+++ utils/models.py
@@ -3,3 +3,23 @@ class Point2D:
         self.x = _x
         self.y = _y
 
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class DoubleLinkNode:
+    def __init__(self, _key, _val, _prev=None, _next=None):
+        self.key = _key
+        self.val = _val
+        self.prev = _prev
+        self.next = _next
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
