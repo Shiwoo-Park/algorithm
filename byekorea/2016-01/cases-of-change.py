dfef76107c5580768c24eeaee147557e1433f64d
shiwoopark <shiwoopark@linecorp.com>
2016-01-26 11:48:20 +0900

    modds

---
+++ byekorea/2016-01/cases-of-change.py
@@ -0,0 +1,7 @@
+# return the number of cases to make the price by given coin information
+# <INPUT>
+# price : 0 < T <= 10000
+# number of kinds of coin : 0 < k <= 100
+# coin price , coin count : p (0 < p <= T), n (0 < n <= 100)
+
+
